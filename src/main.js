2a776e3632bc2e6d8a66b3f4f21cd18e996e94c6
xiesiyu <469563428@qq.com>
2023-04-22 18:03:04 +0800

    初始化环境

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css'
+import '@/assets/css/app.css'
+import router from './router'
+
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
